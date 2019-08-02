afc974c92c3f4429cc6c10b9f0b58df14a3dd56b
mansi <mansi.chourasia@cgi.com>
2019-08-02 17:54:59 +0530

    updated track.java with lombok

--- src/main/java/com/stackroute/trackservice/domain/Track.java
+++ src/main/java/com/stackroute/trackservice/domain/Track.java
@@ -1,54 +1,27 @@
 package com.stackroute.trackservice.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
-
+//@Entity annotation defines that a class can be mapped to a table.
 @Entity
+//@Data is like having implicit @Getter,
+// @Setter, @ToString, @EqualsAndHashCode and @RequiredArgsConstructor annotations on the class
+@Data
+//to generate no argument constructor
+@NoArgsConstructor
+//to generate all parameter argument constructor
+@AllArgsConstructor
+//@Builder lets you automatically produce the code required to have your class be instantiable with code
+@Builder
 public class Track {
     @Id
     private int id;
     private String name;
     private String comments;
 
-    public Track(int id, String name, String comments) {
-        this.id = id;
-        this.name = name;
-        this.comments = comments;
-    }
-
-    public Track() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getComments() {
-        return comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
-
-    @Override
-    public String toString() {
-        return "Track{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", comments='" + comments + '\'' +
-                '}';
-    }
 }
